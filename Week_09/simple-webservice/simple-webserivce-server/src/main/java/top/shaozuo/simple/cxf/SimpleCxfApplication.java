679b6e930198d043658af7c56fd52fbf13efd14a
醉引花眠 <695267004@qq.com>
2021-04-07 20:52:46 +0800

    week09 实现简单的 WebService-CXF 远程调用 demo

---
+++ Week_09/simple-webservice/simple-webserivce-server/src/main/java/top/shaozuo/simple/cxf/SimpleCxfApplication.java
@@ -0,0 +1,12 @@
+package top.shaozuo.simple.cxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCxfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCxfApplication.class, args);
+	}
+}
---
+++ Week_09/simple-webservice/simple-webservice-client/src/main/java/top/shaozuo/simple/cfx/client/SimpleCxfClientApplication.java
@@ -0,0 +1,12 @@
+package top.shaozuo.simple.cfx.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCxfClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCxfClientApplication.class, args);
+	}
+}
